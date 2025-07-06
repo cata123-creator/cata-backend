1629a3ace815db8dff169291eb49159f344035d5
cata123-creator <nailscata952@gmail.com>
2025-07-06 14:03:23 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://nailscata1.netlify.app'
+}));
 app.use(express.json());
 
 // Configuración de la conexión a la base de datos PostgreSQL
